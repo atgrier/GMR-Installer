041072221b27d9321ef5a9cbcd3096093a774f89
Alan Grier <griera@oregonstate.edu>
2021-04-11 20:44:55 -0700

    stub out arduino sketch operations

--- loco_prog/Program.cs
+++ loco_prog/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO
 using ArduinoUploader;
 using ArduinoUploader.Hardware;
 
@@ -19,6 +20,45 @@ namespace loco_prog
             //        ArduinoModel = ArduinoModel.UnoR3
             //    });
             //uploader.UploadSketch();
+        }
+    }
+
+    public class ArudinoSketch
+    {
+        private readonly string sketch_path;
+        private readonly string header_path;
+        private readonly string parameter_path;
+
+        public ArudinoSketch(string sketch_folder, string sketch_name)
+        {
+            sketch_path = Path.Combine(sketch_folder, string.Concat(sketch_name, ".ino"));
+            header_path = Path.Combine(sketch_folder, string.Concat(sketch_name, ".h"));
+            parameter_path = Path.Combine(sketch_folder, string.Concat(sketch_name, ".xml"));
+
+            ReadSketch();
+            GetChanges();
+            MakeChanges();
+            SaveSketch();
+        }
+
+        private void ReadSketch()
+        {
+
+        }
+
+        private void GetChanges()
+        {
+
+        }
+
+        private void MakeChanges()
+        {
+
+        }
+
+        private void SaveSketch()
+        {
+
         }
     }
 }
