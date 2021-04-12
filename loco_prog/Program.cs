26e95005c348bf2dcfce27e62a635eb7e5d680cd
Alan Grier <griera@oregonstate.edu>
2021-04-11 20:38:54 -0700

    comment out old parts

--- loco_prog/Program.cs
+++ loco_prog/Program.cs
@@ -8,16 +8,16 @@ namespace loco_prog
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            var filename = @"loco_code.ino.hex";
-            var portname = "COM7";
-            var uploader = new ArduinoSketchUploader(
-                new ArduinoSketchUploaderOptions()
-                {
-                    FileName = filename,
-                    PortName = portname,
-                    ArduinoModel = ArduinoModel.UnoR3
-                });
+            //Console.WriteLine("Hello World!");
+            //var filename = @"loco_code.ino.hex";
+            //var portname = "COM7";
+            //var uploader = new ArduinoSketchUploader(
+            //    new ArduinoSketchUploaderOptions()
+            //    {
+            //        FileName = filename,
+            //        PortName = portname,
+            //        ArduinoModel = ArduinoModel.UnoR3
+            //    });
             //uploader.UploadSketch();
         }
     }
