d902a739940f2109649cf2a7fb21440bad5f829a
Alan Grier <griera@oregonstate.edu>
2021-04-14 20:47:41 -0700

    start of xml parsing

--- loco_prog/Program.cs
+++ loco_prog/Program.cs
@@ -3,7 +3,6 @@ using System.IO;
 using System.Reflection;
 using System.Xml;
 using System.Collections.Generic;
-using loco_prog.Properties;
 //using ArduinoUploader;
 //using ArduinoUploader.Hardware;
 
@@ -11,7 +10,7 @@ namespace loco_prog
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             //Console.WriteLine("Hello World!");
             //var filename = @"loco_code.ino.hex";
@@ -25,78 +24,98 @@ namespace loco_prog
             //    });
             //uploader.UploadSketch();
 
-            var assembly = Assembly.GetExecutingAssembly();
-            using Stream stream = assembly.GetManifestResourceStream("loco_prog.main_scripts.controller.controller.h");
-            using StreamReader reader = new StreamReader(stream);
+            string[] sketches = { "controller", "receiver" };
+            foreach (string sketch in sketches)
             {
-                string result = reader.ReadToEnd();
-                Console.WriteLine(result);
+                new ArudinoSketch(sketch);
             }
-
-            //string[] sketches = { "controller", "receiver" };
-            //foreach (string sketch in sketches)
-            //{
-            //    new ArudinoSketch("main_scripts", sketch);
-            //}
         }
     }
 
-    //public class ArudinoSketch
-    //{
-    //    private readonly string sketch_path;
-    //    private readonly string header_path;
-    //    private readonly string parameter_path;
+    public class ArudinoSketch
+    {
+        private readonly string sketch_name;
 
-    //    private readonly string sketch_ref;
-    //    private readonly string header_ref;
-    //    private readonly string parameters_ref;
+        private string sketch_ref;
+        private string header_ref;
+        private string parameters_ref;
 
-    //    private string sketch_mod;
-    //    private string header_mod;
-    //    private Dictionary<string, Tuple<Type, string>> parameters_mod;
+        private string sketch_mod;
+        private string header_mod;
+        private Dictionary<string, Tuple<Type, string>> parameters_mod;
 
-    //    public ArudinoSketch(string sketch_folder, string sketch_name)
-    //    {
-    //        sketch_path = Path.Combine(sketch_folder, string.Concat(sketch_name, ".ino"));
-    //        header_path = Path.Combine(sketch_folder, string.Concat(sketch_name, ".h"));
-    //        parameter_path = Path.Combine(sketch_folder, string.Concat(sketch_name, ".xml"));
+        public ArudinoSketch(string name)
+        {
+            sketch_name = name;
 
-    //        ReadSketch();
-    //        GetChanges();
-    //        MakeChanges();
-    //        SaveSketch();
-    //    }
+            ReadSketch();
+            GetChanges();
+            MakeChanges();
+            SaveSketch();
+        }
 
-    //    public ArudinoSketch(string sketch, string header, string parameters)
-    //    {
-    //        sketch_ref = sketch;
-    //        header_ref = header;
-    //        parameters_ref = parameters;
+        private string ReadFile(string extension)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            using Stream stream = assembly.GetManifestResourceStream(
+                $"loco_prog.main_scripts.{ sketch_name}.{sketch_name}.{extension}");
+            using StreamReader reader = new StreamReader(stream);
+                return reader.ReadToEnd();
+        }
 
-    //        GetChanges();
-    //        MakeChanges();
-    //        SaveSketch();
-    //    }
+        private void ReadSketch()
+        {
+            sketch_ref = ReadFile("ino");
+            header_ref = ReadFile("h");
+            parameters_ref = ReadFile("xml");
+        }
 
-    //    private void ReadSketch()
-    //    {
-    //        Console.WriteLine(Resources.ResourceManager.);
-    //        Console.WriteLine(sketch_path);
-    //    }
+        private string GetChange(XmlNode xmlNode)
+        {
+            string value = xmlNode.SelectSingleNode("value").Value;
+            Console.WriteLine(
+                $"Input value for the variable: <{xmlNode.SelectSingleNode("name").Value}>. " +
+                $"If left blank, the default ({value}) will be used.");
+            value ??= Console.ReadLine();
+            Console.WriteLine(value);
+            return value;
+        }
 
-    //    private void GetChanges()
-    //    {
-    //        Console.WriteLine(header_path);
-    //    }
+        private void GetChanges()
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(parameters_ref);
+            XmlNodeList parentNode = xmlDoc.GetElementsByTagName("item");
+            foreach (XmlNode childNode in parentNode)
+            {
+                Type type;
+                Console.WriteLine(childNode.SelectSingleNode("name").Value);
+                switch (childNode.SelectSingleNode("type").Value)
+                {
+                    case "int":
+                        type = typeof(int);
+                        break;
+                    case "float":
+                        type = typeof(float);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid type encountered, defaulting to int.");
+                        type = typeof(int);
+                        break;
+                }
+                string value = GetChange(childNode);
+                parameters_mod[childNode.SelectSingleNode("name").Value] = new Tuple<Type, string>(type, value);
+            }
+        }
 
-    //    private void MakeChanges()
-    //    {
-    //        Console.WriteLine(parameter_path);
-    //    }
+        private void MakeChanges()
+        {
+            Console.WriteLine("making changes");
+        }
 
-    //    private void SaveSketch()
-    //    {
-    //        Console.WriteLine("saving file");
-    //    }
-    //}
+        private void SaveSketch()
+        {
+            Console.WriteLine("saving file");
+        }
+    }
 }
