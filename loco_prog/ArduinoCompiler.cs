60a3929e3d99ea9e73475516b2cfcc9464adb9e3
Alan Grier <griera@oregonstate.edu>
2021-04-17 21:08:58 -0700

    break out arduinosketch into two files

---
+++ loco_prog/ArduinoCompiler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace loco_prog
+{
+    public partial class ArduinoSketch
+    {
+        private static readonly string build_directory = ".\\build\\";
+
+        private void CompileSketch()
+        {
+            CheckCreateDirectory(build_directory);
+            Console.WriteLine("compiling file");
+        }
+    }
+}
